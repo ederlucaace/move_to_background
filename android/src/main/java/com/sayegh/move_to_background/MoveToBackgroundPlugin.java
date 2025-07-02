849b671bafc402fa0a576cb3822d94e1fc0ae70a
Eder Luca <eder.luca@acessorias.com>
2025-07-02 13:49:24 -0300

    AC-1654 Conversão para utilização do embedding v2

--- android/src/main/java/com/sayegh/move_to_background/MoveToBackgroundPlugin.java
+++ android/src/main/java/com/sayegh/move_to_background/MoveToBackgroundPlugin.java
@@ -4,59 +4,54 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 
 /** MoveToBackgroundPlugin */
 public class MoveToBackgroundPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+
   private static final String CHANNEL_NAME = "move_to_background";
   private MethodChannel channel;
   private static Activity activity;
 
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    if (registrar.activity() != null) {
-      MoveToBackgroundPlugin.activity = registrar.activity();
-    }
-    MoveToBackgroundPlugin plugin = new MoveToBackgroundPlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
+  // === FlutterPlugin ===
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    setupChannel(binding.getBinaryMessenger(), binding.getApplicationContext());
   }
 
   @Override
-  @SuppressWarnings("deprecation")
-  public void onAttachedToEngine(FlutterPluginBinding binding) {
-    setupChannel(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
-  }
-
-  @Override
-  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     teardownChannel();
   }
 
-
+  // === Channel helpers ===
   private void setupChannel(BinaryMessenger messenger, Context context) {
     channel = new MethodChannel(messenger, CHANNEL_NAME);
     channel.setMethodCallHandler(this);
-
   }
 
   private void teardownChannel() {
-    channel.setMethodCallHandler(null);
-    channel = null;
+    if (channel != null) {
+      channel.setMethodCallHandler(null);
+      channel = null;
+    }
   }
 
+  // === MethodCallHandler ===
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("moveTaskToBack")) {
-      if (MoveToBackgroundPlugin.activity != null) {
-        MoveToBackgroundPlugin.activity.moveTaskToBack(true);
+    if ("moveTaskToBack".equals(call.method)) {
+      if (activity != null) {
+        activity.moveTaskToBack(true);
       } else {
         Log.e("MoveToBackgroundPlugin", "moveTaskToBack failed: activity=null");
       }
@@ -66,26 +61,24 @@ public class MoveToBackgroundPlugin implements FlutterPlugin, MethodCallHandler,
     }
   }
 
-
+  // === ActivityAware ===
   @Override
-  public void onAttachedToActivity(ActivityPluginBinding binding)
-  {
-    MoveToBackgroundPlugin.activity = binding.getActivity();
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    activity = binding.getActivity();
   }
 
   @Override
   public void onDetachedFromActivityForConfigChanges() {
-    MoveToBackgroundPlugin.activity = null;
+    activity = null;
   }
 
   @Override
   public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
-    MoveToBackgroundPlugin.activity = binding.getActivity();
+    activity = binding.getActivity();
   }
 
   @Override
   public void onDetachedFromActivity() {
-    MoveToBackgroundPlugin.activity = null;
+    activity = null;
   }
-
 }
